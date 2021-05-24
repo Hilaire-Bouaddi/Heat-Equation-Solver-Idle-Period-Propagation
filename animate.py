a0fdb1a2f2e0f4a58a839c847e857d5c8cccc3ad
Hilaire Bouaddi <hilaire.bouaddi@gmail.com>
2021-05-24 18:54:29 +0200

    continue animation

--- animate.py
+++ animate.py
@@ -4,12 +4,25 @@ import sys
 
 with open(sys.argv[1]) as f:
     contents = f.read()
-    NL = float(contents.splitlines()[0].split()[0])
-    NH = float(contents.splitlines()[0].split()[1])
-    N_ITER = float(contents.splitlines()[0].split()[2])
+    NL = int(contents.splitlines()[0].split()[0])
+    NH = int(contents.splitlines()[0].split()[1])
+    N_ITER = int(contents.splitlines()[0].split()[2])
 
     T = []
     
-    for n in [1]:
-        print(contents.splitlines()[NH*n:NH*(n+1)])
-        
+    for n in range(N_ITER):
+        T_n = []
+        for line in contents.splitlines()[NH*n+1:NH*(n+1)+1]:
+            T_n.append(list(map(float, line.split())))
+        T.append(T_n)
+    
+T = np.array(T)
+
+fig, ax = plt.subplots()
+
+for n in range(N_ITER):
+    ax.cla()
+    im = ax.imshow(T[n], cmap=plt.get_cmap('coolwarm'), vmin=0,vmax=100)
+    ax.set_title("frame {}".format(n))
+    # Note that using time.sleep does *not* work here!
+    plt.savefig("frames/frame {}".format(n))
