35a4d93d6231aaab759f25b1ff91c17a7abe058b
Hilaire Bouaddi <hilaire.bouaddi@gmail.com>
2021-06-01 16:37:04 +0200

    progress animation

--- animate.py
+++ animate.py
@@ -11,17 +11,19 @@ with open(sys.argv[1]) as f:
     dt = float(contents.splitlines()[0].split()[3])
 
     T = []
-    
+    print(N_ITER) 
     for n in range(N_ITER):
         T_n = []
         for line in contents.splitlines()[NH*n+1:NH*(n+1)+1]:
             T_n.append(list(map(float, line.split())))
         T.append(T_n)
-    
+
+
 T = np.array(T)
 fig, ax = plt.subplots()
 
 frames_names = []
+
 for n in range(N_ITER):
     ax.cla()
     im = ax.imshow(T[n], cmap=plt.get_cmap('coolwarm'), vmin=0,vmax=100)
@@ -33,8 +35,8 @@ for n in range(N_ITER):
     #plt.close()
 
 
-fps = int(1/dt) * 2
-with imageio.get_writer('animation.gif', mode='I', fps=fps) as writer:
+fps = 1/dt
+with imageio.get_writer('animation_parallel.gif', mode='I', fps=fps) as writer:
     for filename in frames_names:
         image = imageio.imread(filename)
         writer.append_data(image)
